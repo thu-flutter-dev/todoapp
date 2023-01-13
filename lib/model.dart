d8c5c06b3f92bfa334b15fd1df43290185f5083b
yxj <564197835@qq.com>
2023-01-13 15:09:40 +0800

    update list

--- lib/model.dart
+++ lib/model.dart
@@ -34,30 +34,4 @@ class TodoListModel extends ChangeNotifier {
   }
 }
 
-List<String> defaultTodoContents = [
-  "Todo0",
-  "Todo1",
-  "Todo2",
-  "Todo3",
-  "Todo4",
-  "Todo5",
-  "Todo6",
-  "Todo7",
-  "Todo8",
-  "Todo9",
-  "Todo10",
-  "Todo11",
-  "Todo12",
-  "Todo13",
-  "Todo14",
-  "Todo15",
-  "Todo16",
-  "Todo17",
-  "Todo18",
-  "Todo19",
-  "Todo20",
-  "Todo21",
-  "Todo22",
-  "Todo23",
-  "Todo24",
-];
+List<String> defaultTodoContents = List.generate(25, (i) => "Todo $i");
